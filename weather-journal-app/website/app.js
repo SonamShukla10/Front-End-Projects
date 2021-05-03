0c732ad184bf3728b0b0df2b2a8e04ad26964200
Sonam Shukla <48238169+SonamShukla10@users.noreply.github.com>
2021-05-03 15:24:58 -0700

    Update app.js

--- weather-journal-app/website/app.js
+++ weather-journal-app/website/app.js
@@ -1,80 +1,104 @@
 /* Global Variables */
-let baseUrl = 'http://api.weatherinfo.org/data/1.0/weather?';
-const apiKey = 'FXjLYVAr0qoqFfsbLXcUVyficEaKl0r8VixF5LqG';
+ let baseUrl ='api.openweathermap.org/data/2.5/weather?q=';
+ // 'http://api.openweathermap.org/data/2.5/weather?newWeather=';
+let apiKey = 'Sonam&appid=29ffe73e4218f0e4dce19128713ad41d';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate =  d.toLocaleString('en-US', { month: 'long', day: 'numeric', year: 'numeric' });
 
 
-document.getElementById('generate').addEventListener('click', clickAction);
-function clickAction(ele) {
-  const pinCode = document.getElementById('zip').value;
-  const feelings = document.getElementById('feelings').value;
-  console.log(newDate);
-  getTemp(baseUrl, pinCode, apiKey)
-  .then(function (data) {
-    allData('http://localhost:8800/addWeatherData', {temp: data.main.temp, location: data.main.location, date: newDate, user_response: feelings } )
-        .then(function() {
-            updatesUserInterface()
-        })
-    })
-}
+document.getElementById('generate').addEventListener('click', performAction);
 
-//async
-const getTemp = async(baseUrl, code, apiKey)=> {
-  const response = await fetch(baseUrl + code + ',us' + '&appId=' + apiKey)
-  console.log(response);
-  try {
-    const data = await response.json();
+function performAction(e){
+const newWeather =  document.getElementById('weather');
+const feelings = document.getElementById('feelings');
+
+getWeather(baseUrl ,newWeather , apiKey )
+.then (function(data) {
     console.log(data);
-    console.log('SONAM');
+    postData('/addWeather' ,{temp:data.main.temp ,date:newDate, feeling:feeling})
+    //{`${baseUrl}newWeather=${weather}&appid=${apiKey}`} )
+  })
+  .then(
+      updateInfo()
+    )
+
+  };
+
+
+const getWeather = async (baseUrl, newWeather, key)=>{
+  const res = await fetch(baseUrl+newWeather+apiKey)
+  try {
+
+    const data = await res.json();
+    console.log(data)
     return data;
-  }
-  catch(error) {
-    console.log("Error", error);
-  }
-}
-
-const allData = async (url = '', data = {})=> {
-  const allRequest = await fetch(url, {
-    method: 'POST',
-    credentials: 'same-origin',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data);
-  });
-  try {
-    console.log('PARA');
-    const newData = await allRequest.json();
-    console.log(newData, 'SONAM PARA');
-    return newData;
-  }
-  catch(error) {
-    console.log("Error", error);
-  }
-}
-
-//updates
-const updatesUserInterface = async () => {
-  const request = await fetch('http://localhost:8800/all');
-  try {
-        const fullData = await request.json();
-        console.log('AREA');
-        document.getElementById('date').innerHTML = fullData.date;
-        document.getElementById('temp').innerHTML = fullData.temp;
-        document.getElementById('content').innerHTML = fullData.user_response;
+    postData('/add', {
+      location : data.main.location,
+      temp: data.main.temp,
+      date: newDate(),
+      feelings: data.main.feelings,
+        })
     }
-    catch (error) {
+    catch(error) {
+      console.log("error", error);
+    // appropriately handle the error
+  }
+};
+
+
+// ######################################################
+const postData = async ( url = '', data = {})=>{
+
+      const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      credentials: 'same-origin',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data), // body data type must match "Content-Type" header
+    });
+
+      try {
+        const newData = await response.json();
+               return newData
+      }catch(error) {
+      console.log("error", error);     // error handling
+      }
+  }
+
+
+// ########################################################
+const newData = async (url='') =>{
+    const req = await fetch(url);
+    try {
+        const newData = await req.json()
+    }
+    catch(error){
         console.log('error', error);
     }
+};
+
+
+function tempConversion(kelvin) {
+    let celsius = Math.floor(kelvin - 273)
+    return `${celsius}°C`
 }
-}
 
 
 
-
-
-  })
-}
+const updateInfo = async (url = '') => {
+    const req = await fetch('/all');
+    try {
+        const fullData = await req.json();
+        document.getElementById('date').innerHTML = fullData.date;
+        document.getElementById('temp').innerHTML = tempConversion(fullData.temp);
+        document.getElementById('content').innerHTML = `Your feeling: ${fullData.feelings}`;
+        document.getElementById('location').innerHTML = fullData.location;
+        console.log(fullData)
+    } catch(error) {
+        console.log("error", error);
+    }
+  }
+// ########################################################
