fbb60570ab1e2a47fc290d70755da49ccc9b6f6d
Sonam Shukla <48238169+SonamShukla10@users.noreply.github.com>
2021-06-07 13:48:04 -0700

    Add files via upload

---
+++ evaluate-news-nlp project/src/client/js/nameChecker.js
@@ -0,0 +1,14 @@
+function checkForName(inputText) {
+  console.log("::: Running checkForName :::", inputText);
+  const r = new RegExp(/^(http|https):\/\/[^ "]+$/);
+  return r.test(inputText);
+    // let names = [
+    //     "Picard",
+    //     "Janeway",
+    //     "Kirk",
+    //     "Archer",
+    //     "Georgiou"
+    // ]
+}
+
+export { checkForName }
